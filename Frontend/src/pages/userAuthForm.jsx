03a2bb96ae605bb4958b6e5065a54ebd81e6ed6a
hadi-vbout <hadi.hayek@vbout.com>
2023-11-30 02:52:31 +0200

    [Enabled frontend-backend integration

--- Frontend/src/pages/userAuthForm.jsx
+++ Frontend/src/pages/userAuthForm.jsx
@@ -1,13 +1,69 @@
-import React from "react";
+import React, { useRef } from "react";
 import Input from "../components/input";
 import googleIcon from '../imgs/google.png'
 import { Link } from "react-router-dom";
-import AnimationWrapper from '../common/pageanimation'
+import AnimationWrapper from '../common/pageanimation';
+import {Toaster,toast} from 'react-hot-toast';
+import axios from 'axios'
+
+
 export default function userAuthForm({ type }) {
+
+
+   const userAuthServer = (serverRoute, formData) => {
+    axios.post('http://localhost:3000' + serverRoute, formData)
+      .then(({ data }) => {
+        console.log(data);
+      })
+      .catch(({ response }) => {
+        toast.error(response?.data?.error || 'An error occurred');
+      });
+  };
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+
+    let serverRoute= type == 'sign-in' ? '/signin' : '/signup';
+    console.log('Server Route:', serverRoute);
+
+    let emailRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/
+    let passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()\-_=+{};:,<.>]).{8,}$/;
+    
+  
+
+    let form= new FormData(formElement);
+    let formData={};
+
+    for (let[key,value] of form.entries()){
+      formData[key]=value;
+    }
+
+    let {fullname,email,password}=formData;
+
+    if (type !== 'sign-in' &&fullname.length < 3) {
+      return toast.error( "Fullname must be at least 3 letters" )
+  }
+  if (!email.length) {
+      return toast.error(  " Email cannot be empty" )
+  }
+  if (!emailRegex.test(email)) {
+      return toast.error( "Email is invalid" )
+
+  }
+  if (!passwordRegex.test(password)) {
+      return toast.error( "Password must be 8+ characters with at least 1 uppercase, 1 lowercase, 1 digit, and 1 special character " )
+
+  }
+
+  userAuthServer(serverRoute,formData);
+
+  }
+
   return (
     <AnimationWrapper keyVal={type}>
     <section className="h-cover flex items-center justify-center">
-      <form className="w-[88%] max-w-[480px]">
+      <Toaster/>
+      <form id='formElement' className="w-[88%] max-w-[480px]" >
         <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
           {type == "sign-in" ? "Welcome back" : "Join Us"}
         </h1>
@@ -37,6 +93,7 @@ export default function userAuthForm({ type }) {
 
         <button className="btn-dark center mt-14"
         type="submit"
+        onClick={handleSubmit}
         >
             { type.replace('-','') }
 
